484fcfead4270aff52b1c2d5a9607832fcf14d60
Maksim Zakharchuk <mad_max0100@mail.ru>
2022-11-26 21:15:26 +0300

    feature: добавить валидацию всех форм

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,7 +1,3 @@
-const form = document.querySelector('.popup__form');
-const formInput = form.querySelector('.popup__input');
-const formError = form.querySelector(`.${formInput.id}-error`);
-
 const showInputError = (formElement, inputElement, errorMessage) => {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
   inputElement.classList.add('popup__input_type_error');
@@ -24,11 +20,30 @@ const checkInputValidity = (formElement, inputElement) => {
   }
 };
 
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+  return !inputElement.validity.valid;
+  });
+}
+
+const toggleButtonState = (inputList, buttonElement) => {
+  if (hasInvalidInput(inputList)) {
+  buttonElement.setAttribute('disabled', '');
+  buttonElement.classList.add('popup__save-button_disabled');
+} else {
+  buttonElement.removeAttribute('disabled');
+  buttonElement.classList.remove('popup__save-button_disabled');
+}
+}
+
 const setEventListeners = (formElement) => {
   const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+  const buttonElement = formElement.querySelector('.popup__save-button');
+  toggleButtonState(inputList, buttonElement);
   inputList.forEach((inputElement) => {
     inputElement.addEventListener('input', function () {
       checkInputValidity(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
     });
   });
 }
